e8dea6c6791b963c63d205124051c177b2b55191
Saxon Fletcher <saxon@projuce.com>
2014-05-10 14:10:55 +1000

    responsive changes

--- js/script.js
+++ js/script.js
@@ -33,16 +33,38 @@ $(function() {
 	});
 
 	/* Flexslider for homepage/lookbook */
-    $('.full-slider').flexslider({
-    	directionNav: false
-    });
+  $('.full-slider').flexslider({
+  	directionNav: false
+  });
 
-    /* Filter Dropdown */
-    $(".toggle-filter").on("click", function(){
-    	$(".filter-bar").slideToggle();
-    	return false;
-    });
+  /* Filter Dropdown */
+  $(".toggle-filter").on("click", function(){
+  	$(".filter-bar").slideToggle();
+  	return false;
+  });
 
+  /* Responsive menu */
+  $(".nav-toggle").on("click", function(){
+  	$(this).toggleClass("active");
+  	$(".site-nav").toggleClass("active");
+  });
+
+  /* Product image slider on mobile */
+  if ( jQuery(window).width() < 768 ) {
+    $('.mobile-slider').flexslider({
+      directionNav: false
+    });
+  }
+
+});
+
+/* Product image slider when resizing */
+jQuery(window).resize( function () {
+  if ( jQuery(window).width() < 768 ) {
+    $('.mobile-slider').flexslider({
+      directionNav: false
+    });
+  }
 });
 
 
